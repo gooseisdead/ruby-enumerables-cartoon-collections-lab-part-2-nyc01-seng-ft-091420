b9a6c210f70a015118d6fb2792bdf99a6bbc7f05
Mike Smith <gooseisdead@mac.com>
2020-09-08 00:45:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -35,6 +35,5 @@ def find_valid_calls(planeteer_calls)
         # binding.pry
       end
     end
-  end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
